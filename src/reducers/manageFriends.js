443162226d1c3d48fff9f5bae8de75fa82893d9a
Jahaziel Guzman <jahaziel.guzman93@gmail.com>
2019-09-13 18:34:16 -0400

    made redux reducers

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,11 @@
 export function manageFriends(state, action){
+  // action.friend ?
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return {...state, friends: [...state.friends, action.friend]};
+    case "REMOVE_FRIEND":
+      return {...state, friends: state.friends.filter(r => r.id != action.id)};
+    default:
+      return state;
+  }
 }
