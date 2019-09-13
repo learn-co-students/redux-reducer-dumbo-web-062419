f30c3f35a0bee37570c906d8a0964f71858ca990
lgm527 <laurellmccaffrey@gmail.com>
2019-09-13 10:55:18 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,11 @@
 export function manageFriends(state, action){
+  switch(action.type){
+    case 'ADD_FRIEND':
+      return {friends: [...state.friends, action.friend]}
+    case 'REMOVE_FRIEND':
+      const byeFriend = state.friends.findIndex(friend => friend.id === action.id)
+      return {friends: [...state.friends.slice(0, byeFriend), ...state.friends.slice(byeFriend + 1)]}
+    default:
+      return state;
+  }
 }
